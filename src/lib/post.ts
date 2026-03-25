8de5a00ee063cfa913bf44d4e3685484d1fac9f6
Masaya Yamamoto <masaya1122baseball@gmail.com>
2026-03-25 21:59:39 +0900

    feat: 記事詳細作成

--- src/lib/post.ts
+++ src/lib/post.ts
@@ -13,3 +13,16 @@ export async function getPosts() {
         orderBy: { createdAt: "desc" },
     });
 }
+
+export async function getPost(id: string) {
+    return await prisma.post.findUnique({
+        where: { id },
+        include: {
+            author: {
+                select: {
+                    name: true,
+                },
+            },
+        },
+    });
+}
