4baec7e2c5682c99cb90184ecd195096eb9e014f
Masaya Yamamoto <masaya1122baseball@gmail.com>
2026-04-30 20:21:32 +0900

    feat: 記事投稿作成

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      }
-    ]
-  }
+    /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "picsum.photos",
+            },
+        ],
+    },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: "5mb", // 必要に応じて値を変更
+        },
+    },
 };
 
 export default nextConfig;
